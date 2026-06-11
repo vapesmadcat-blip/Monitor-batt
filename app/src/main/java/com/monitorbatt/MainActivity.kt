e5e8a1ce9353c152e67604016c872688ddd200d4
vapesmadcat-blip <vapesmadcat@gmail.com>
2026-06-10 21:44:40 -0300

    Delete app directory

--- app/src/main/java/com/monitorbatt/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-
-package com.monitorbatt
-import android.app.Activity
-import android.os.Bundle
-class MainActivity: Activity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-  }
-}
